068255958b0f2870c9aed0be85ca88fae66a2355
goom <30990201+MarkMrGamer@users.noreply.github.com>
2021-10-03 15:12:59 -0700

    added files

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>nebspecies - Home</title>
+    <link rel="stylesheet" type="text/css" href="mainpage.css">
+</head>
+
+<body>
+    <table border="0" width="100%" height="100%">
+        <tbody>
+            <tr>
+                <td valign="top" width="250" bgcolor="#8b4cb5">
+                    <img src="images/navlogo.png">
+                </td>
+                <td bgcolor="#5e2f7d">
+
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+<html>
