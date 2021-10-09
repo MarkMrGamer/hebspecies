fcaace4ef8fb3dc8d6a4dfeb6f9d9c483753c520
goom <30990201+MarkMrGamer@users.noreply.github.com>
2021-10-09 10:56:02 -0700

    Div problem

---
+++ container/navbar.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>hebspecies - navbar</title>
+		<link rel="stylesheet" type="text/css" href="../mainpage.css">
+    </head>
+    <body>
+        <img src="../images/navlogo.png">
+    </body>
+</html>
--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 		<title>nebspecies - Home</title>
 		<link rel="stylesheet" type="text/css" href="mainpage.css">
 		</head>
-
 		<body>
-			<div id="container">
-				<div id="sidebar">
-					<img src="images/navlogo.png">
-					</div>
-					<div id="frame">
-						<iframe src="container/main.php" id="frame2"/>
-					</div>
-				</div>
-			</body>
-		</html>
+			<table width="100%" height="100%">
+				<tr>
+					<td width="252" valign="top" bgcolor="#8b4cb5">
+						<iframe src="container/navbar.php" class="frame1" width="252" height="100%" align="left" frameborder="0"/></iframe>
+					</td>
+					<td valign="top" bgcolor="#5e2f7d">
+						<iframe src="container/main.php" class="frame2" width="100%" height="100%" align="left" frameborder="0"/></iframe>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
