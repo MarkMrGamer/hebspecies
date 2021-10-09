a3355e2e84d1a9be283520b6a46e781a61981851
goom <30990201+MarkMrGamer@users.noreply.github.com>
2021-10-08 21:31:01 -0700

    More site changes + additions
    some stuff

---
+++ container/main.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>hebspecies - home</title>
+		<link rel="stylesheet" type="text/css" href="page.css">
+		</head>
+		<body>
+			<h1>main page</h1>
+		</body>
+	</html>
--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <html>
+	<head>
+		<title>nebspecies - Home</title>
+		<link rel="stylesheet" type="text/css" href="mainpage.css">
+		</head>
 
-<head>
-    <title>nebspecies - Home</title>
-    <link rel="stylesheet" type="text/css" href="mainpage.css">
-</head>
-
-<body>
-    <table border="0" width="100%" height="100%">
-        <tbody>
-            <tr>
-                <td valign="top" width="250" bgcolor="#8b4cb5">
-                    <img src="images/navlogo.png">
-                </td>
-                <td bgcolor="#5e2f7d">
-
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</body>
-<html>
+		<body>
+			<div id="container">
+				<div id="sidebar">
+					<img src="images/navlogo.png">
+					</div>
+					<div id="frame">
+						<iframe src="container/main.php" id="frame2"/>
+					</div>
+				</div>
+			</body>
+		</html>
